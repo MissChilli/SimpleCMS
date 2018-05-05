c1adfdc3e96585c962d9b6ff6f10925635ead9be
yyyy <yy@yyyDESKTOP-D7R0BES>
2018-05-05 16:15:29 +0800

    5

--- SimpleCMS/Global.asax.cs
+++ SimpleCMS/Global.asax.cs
@@ -7,12 +7,58 @@ using System.Web.Routing;
 
 namespace SimpleCMS
 {
+    using SimpleCMS.Helper;
+    using SimpleCMS.LocalResources;
+    using SimpleCMS.Migrations;
+    using SimpleCMS.Models;
+    using System.Data.Entity;
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            bool isAjaxCall = string.Equals("XMLHttpRequest", Context.Request.Headers["x-requested-with"],
+                StringComparison.OrdinalIgnoreCase);
+            var exception = Server.GetLastError();
+            Response.Clear();
+            var httpStatusCode = (exception as HttpException)?.
+                                 GetHttpCode() ?? 500;
+            string msg = "";
+            switch (httpStatusCode)
+            {
+                case 403:
+                    msg = Message.Forbid403;
+                    break;
+                case 404:
+                    msg = Message.NotFound404;
+                    break;
+                case 500:
+                    msg = Message.InternalServerError500;
+                    break;
+                default:
+                    msg = Message.OtherError;
+                    break;
+            }
+
+            Server.ClearError();
+            if (isAjaxCall)
+            {
+                Response.Write(ExtJs.WriterJObject(false, msg: msg).ToString()); ;
+            }
+            else
+            {
+                Response.Charset = "utf-8";
+                Response.Write(msg);
+            }
+            Response.End();
+
         }
     }
 }
---
+++ SimpleCMS/Models/Tag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCMS.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("T_Tag")]
+    public class Tag
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [Column(TypeName = "nvarchar")]
+        [Index("IX_TAG_TAGNAME", IsUnique = true)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Content> Contents { get; set; }
+    }
+}
